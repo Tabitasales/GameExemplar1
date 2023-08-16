4c5f8a0d67a59cfe303c2511d8a8b3615df5198c
Tabita Sales <111479692+Tabitasales@users.noreply.github.com>
2023-08-16 14:55:33 +1200

    Movement to initial sprite

--- game.js
+++ game.js
@@ -4,4 +4,24 @@ const ctx = canvas.getContext("2d");
 ctx.shadowBlur = 20;
 ctx.shadowColor = "black";
 ctx.fillStyle = "red";
-ctx.fillRect (20, 20, 100, 80);
+
+let rectX = 20;  // Initial X position of the rectangle
+const rectY = 20;
+const rectWidth = 100;
+const rectHeight = 80;
+const rectSpeed = 2;  // Adjust this for desired movement speed
+
+function drawRectangle() {
+    // Clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Update the X position of the rectangle
+    rectX += rectSpeed;
+
+    // Draw the rectangle with updated position
+    ctx.fillRect(rectX, rectY, rectWidth, rectHeight);
+
+    requestAnimationFrame(drawRectangle);  // Keep animating
+}
+
+drawRectangle();
